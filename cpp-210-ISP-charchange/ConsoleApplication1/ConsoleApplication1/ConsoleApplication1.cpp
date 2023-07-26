f50880a79a4ec1cf14d616b747b55d348cfd4440
James Lind <112474995+Jlindhub@users.noreply.github.com>
2023-07-26 18:57:49 +0200

    char-shifting
    simple program to take in one character and output the character before it according to it's ASCII value.

---
+++ cpp-210-ISP-charchange/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    char input;
+    cout<<"please input a character.";
+    cin>>input;
+    input=input-1;
+    cout<<input;
+    return 0;
+}
