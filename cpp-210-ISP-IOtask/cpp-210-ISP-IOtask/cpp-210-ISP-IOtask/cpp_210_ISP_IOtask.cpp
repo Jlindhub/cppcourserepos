f90f470dd9b4bfc476b6bd6c2e5af91eb890b61f
James Lind <112474995+Jlindhub@users.noreply.github.com>
2023-07-26 18:46:28 +0200

    input/output
    very simple program to show capability of writing input/output in an isolated program.

---
+++ cpp-210-ISP-IOtask/cpp-210-ISP-IOtask/cpp-210-ISP-IOtask/cpp_210_ISP_IOtask.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    int numbergiven=0;
+   
+    cout << "please input your age, i'll tell you how old you'll be a year from now.\n";
+    cin>>numbergiven;
+    if(numbergiven!=0)
+    {
+        cout<<"you will be "<<numbergiven+1;
+    }
+    else
+    {
+        cout<<"i can't help you if you won't give me a numerical value.";
+    }
+}
