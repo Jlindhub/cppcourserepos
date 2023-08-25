9b1c5640835e32cd2e4ae69557b31fef8a316237
James Lind <james.lind@student.forsbergsskola.se>
2023-08-25 12:10:29 +0200

    book type book
    changes function 'int' type to 'book' type to enable further information passing if necessary.

--- cpp-210-ISP-book/bookstealing/bookstealing/bookstealing.cpp
+++ cpp-210-ISP-book/bookstealing/bookstealing/bookstealing.cpp
@@ -7,16 +7,16 @@ struct Book {
     bool hardcover;
 };
 
-int stealHalfTheBook(Book book){
+Book stealHalfTheBook(Book book){
     book.pages /= 2;
     printf("Pages of Book after stealing:%d\n", book.pages); // 100
-    return book.pages;
+    return book;
 }
 
 int main(){
     Book book;
     book.pages = 200;
     printf("Pages of main-Book before stealing:%d\n", book.pages); // 200
-    book.pages = stealHalfTheBook(book); // clone
+    book = stealHalfTheBook(book); // clone
     printf("Pages of main-Book after stealing:%d\n", book.pages); // 200
 }
