3d72bda3b6cf4780cbc3a4d530e8724d3ed88945
James Lind <james.lind@student.forsbergsskola.se>
2023-08-25 12:08:12 +0200

    "steal half the book" fix
    changes steal half the book to have return type int and uses the returned value to overwrite the main-book's value so that the number of pages is split properly.

--- cpp-210-ISP-book/bookstealing/bookstealing/bookstealing.cpp
+++ cpp-210-ISP-book/bookstealing/bookstealing/bookstealing.cpp
@@ -1,12 +1,22 @@
-void stealHalfTheBook(Book book){
-    book.numberOfPages /= 2;
-    printf("Pages of Book after stealing:%d\n", book.numberOfPages); // 100
+#include <cstdio>
+
+struct Book {
+    char name[256];
+    int year;
+    int pages;
+    bool hardcover;
+};
+
+int stealHalfTheBook(Book book){
+    book.pages /= 2;
+    printf("Pages of Book after stealing:%d\n", book.pages); // 100
+    return book.pages;
 }
 
 int main(){
     Book book;
-    book.numberOfPages = 200;
-    printf("Pages of main-Book before stealing:%d\n", book.numberOfPages); // 200
-    stealHalfTheBook(book); // clone
-    printf("Pages of main-Book after stealing:%d\n", book.numberOfPages); // 200
+    book.pages = 200;
+    printf("Pages of main-Book before stealing:%d\n", book.pages); // 200
+    book.pages = stealHalfTheBook(book); // clone
+    printf("Pages of main-Book after stealing:%d\n", book.pages); // 200
 }
