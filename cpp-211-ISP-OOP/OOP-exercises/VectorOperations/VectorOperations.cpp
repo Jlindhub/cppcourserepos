cb2b18aed274fc515fee595a55118ba3f48c91db
james <jamesoslind@gmail.com>
2023-09-19 18:11:22 +0100

    threevector implementation
    implements threevector, a three dimensional vector class, fully featured with appropriate operators.

--- cpp-211-ISP-OOP/OOP-exercises/VectorOperations/VectorOperations.cpp
+++ cpp-211-ISP-OOP/OOP-exercises/VectorOperations/VectorOperations.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
-
 struct threeVector //was class, but since it's all public by necessity...
 {
 
     int x, y, z;
     threeVector(const int x,const int y,const int z):x{x}, y{y}, z{z} {} //constr. with values
     threeVector():x{0}, y{0}, z{0} {} //empty-constr.
+    explicit threeVector(const int i):x{i}, y{i}, z{i} {} //empty-constr.
 
     //some of these definitions are probably unnecessary. oh well.
     //operator defs
     threeVector operator+(const threeVector& other) const {return threeVector{x+other.x, y+other.y, z+other.z};}
-    threeVector operator-(const threeVector& other) const {return threeVector{x-other.x,y-other.y,z-other.z};}
-    threeVector operator*(const threeVector& other) const {return threeVector{x*other.x, y*other.y,z*other.z};}
+    threeVector operator-(const threeVector& other) const {return threeVector{x-other.x, y-other.y, z-other.z};}
+    threeVector operator*(const threeVector& other) const {return threeVector{x*other.x, y*other.y, z*other.z};}
     threeVector operator/(const threeVector& other) const {return threeVector{x/other.x, y/other.y, z/other.z};}
 
     //single-other defs
@@ -22,16 +22,31 @@ struct threeVector //was class, but since it's all public by necessity...
     threeVector operator/(const int other) const {return threeVector{x/other, y/other,z/other};}
 
     //reference operator defs
-    threeVector operator+=(const threeVector& other) {this->x=x+other.x; this->y=y+other.y; this->z=z+other.z; return *this;}
-    threeVector operator-=(const threeVector& other) {this->x=x-other.x; this->y=y-other.y; this->z=z-other.z; return *this;}
-    threeVector operator*=(const threeVector& other) {this->x=x*other.x; this->y=y*other.y; this->z=z*other.z; return *this;}
-    threeVector operator/=(const threeVector& other) {this->x=x/other.x; this->y=y/other.y; this->z=z/other.z; return *this;}
+    threeVector operator+=(const threeVector& other) {x=x+other.x; y=y+other.y; z=z+other.z; return *this;}
+    threeVector operator-=(const threeVector& other) {x=x-other.x; y=y-other.y; z=z-other.z; return *this;}
+    threeVector operator*=(const threeVector& other) {x=x*other.x; y=y*other.y; z=z*other.z; return *this;}
+    threeVector operator/=(const threeVector& other) {x=x/other.x; y=y/other.y; z=z/other.z; return *this;}
 
     //increment operator defs
-    threeVector& operator++()   {this->x++; this->y++; this->z++; return *this;}
+    threeVector& operator++()   {x++; y++; z++; return *this;}
     threeVector operator++(int) {threeVector const old = *this; operator++(); return old;}
     //ask marc about how/why this^ works - why return old? what do we do with the operator++() return? nothing?
 
+    
+    //equality operator defs (inline redundant?) - too many parameter error?
+    friend auto operator<=>(const threeVector& lhs, const threeVector& rhs) = default;
+    //can't get functional. <=> not a thing in this ver?
+    bool operator==(const threeVector& rhs) const {return x==rhs.x && y==rhs.y && z==rhs.z; }
+    
+    bool operator!=(const threeVector& rhs) const {return !(*this==rhs);}
+    bool operator< (const threeVector& rhs) const {return x<rhs.x && y<rhs.y && z<rhs.z; }
+    bool operator> (const threeVector& rhs) const {return rhs < *this;}
+    bool operator<=(const threeVector& rhs) const {return !(*this > rhs);}
+    bool operator>=(const threeVector& rhs) const {return !(*this < rhs);}
+
+    //cast defs
+    explicit operator int() const { return x+y+z; }
+    
 };
 //other-value first operator definitions for 3vector
 threeVector operator+(const int i, const threeVector& v) {return {v.x+i, v.y+i,v.z+i};}
@@ -39,8 +54,8 @@ threeVector operator-(const int i, const threeVector& v) {return {v.x-i, v.y-i,v
 threeVector operator*(const int i, const threeVector& v) {return {v.x*i, v.y*i,v.z*i};}
 threeVector operator/(const int i, const threeVector& v) {return {v.x/i, v.y/i,v.z/i};}
 
-
-
+class Animal{};
+class Dog : public Animal{};
 
 int main(int argc, char* argv[])
 {
@@ -51,5 +66,9 @@ int main(int argc, char* argv[])
     newVector++;
     newVector = newVector * 2;
     newVector = 2 * newVector;
+
+    threeVector singleVector(3);
+    int i = static_cast<int>(singleVector);
+
     return 0;
 }
