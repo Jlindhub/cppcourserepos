04e5e899213aa96da58578dc98dc6dd971333fbb
James Lind <james.lind@student.forsbergsskola.se>
2023-09-28 16:50:33 +0200

    final commit hopefully!
    implements a fucked version of select sort as well as finishing the averagable class with a concept limiter to make potential errors easier to comprehend.

--- cpp-211-ISP-OOP/OOP-exercises/Polymorphism/Polymorphism.cpp
+++ cpp-211-ISP-OOP/OOP-exercises/Polymorphism/Polymorphism.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
@@ -9,7 +10,43 @@ Tout NarrowCast(Tin input)
     if(revertCheck!=input){throw 69420;}
     return result;
 }
-template<class T, class TTotal>
+
+template<class T> //generic array sorter concept: check for compatability with <=>
+concept sorteable = requires (T input, T input2)
+{
+    input > input2;
+};
+
+//generic array sorter: compare first value of each array
+template<sorteable T>
+void BubbleSort(T* input, size_t length)
+{
+    //idk what this sort is. cyclesort? 
+    T* end = input+length;
+    while(input != end)
+    {
+        T* ptr2=input+1;
+        while(ptr2 != end)
+        {
+            if(*input > *ptr2)
+            {
+                T temp = *input;
+                *input = *ptr2;
+                *ptr2 = temp;
+            }
+            ++ptr2;
+        }
+        ++input;
+    }
+}
+
+
+template<class T>
+concept averagable = requires (T input, size_t length) {
+    input+=input;
+    input/length;
+};
+template<class T, averagable TTotal>
 T tAverage(T* input, size_t arrlength)
 {
     TTotal total = 0;
@@ -21,7 +58,7 @@ T tAverage(T* input, size_t arrlength)
     }
     return total/arrlength;
 }
-template<class T>
+template<averagable T>
 T tAverage(T* input, size_t arrlength)
 {
     return tAverage<T, T>(input, arrlength);
@@ -41,14 +78,30 @@ int main(int argc, char* argv[])
     double averaged = tAverage(arr, size(arr));
     char chararr[5] = {'a','b','c','d','e'};
     char chararrav = tAverage(chararr,size(chararr));
-    const char* names[5] = {"Hello", "World"};
-    bool average3 = tAverage(names, size(names));
 
     cout << averaged << endl;
     cout << chararrav << endl;
-    cout << average3 << endl;
+
+    int sorttest[5] = {4,5,2,3,1};
+    cout << sorttest << endl;
+    BubbleSort(sorttest,size(sorttest));
+    for (int i : sorttest)
+    {
+        cout << i;
+    }
+
+    string stringtest[5] = {"Hello", "World", "Test", "Foo", "Six"};
+    cout << stringtest << endl;
+    BubbleSort(stringtest,size(stringtest));
+    for (string i : stringtest)
+    {
+        cout << i;
+    }
+    cout << endl;
+
     return 0;
 }
+
 
 
 
@@ -66,6 +119,6 @@ int main() {
     int i = 5;
     sqr(i);
     sqr(&i); // ERROR
-}
+}   
 
 */
